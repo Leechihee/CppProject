c23ec7be9cf902bf79539facc3e30e8a11ca37c7
Leechihee <silent7456@naver.com>
2025-04-04 05:35:57 +0900

    파일 입출력 사용 안할거기때문에 Seller, Customer 계정 미리 생성해두기

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,17 +1,8 @@
 ﻿#include "shop.h"
 
-
 int main()
 {
-	Customer test("1", "2", "3");
-
-	Shop s;
-	s.displayThingsList(test);
-	s.pushThingsToList(Things("칫솔", 100, 1000));
-	s.displayThingsList(test);
-	s.pushThingsToList(Things("핸드폰", 50, 750));
-	s.displayThingsList(test);
-	s.popThingsToList();
-	s.displayThingsList(test);
+	Seller seller("Admin", "admin", "00000000");
+	Customer customer("Customer", "customer", "99999999");
 	return 0;
 }
